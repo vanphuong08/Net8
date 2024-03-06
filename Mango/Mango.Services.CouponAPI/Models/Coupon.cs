4a4e86b3430205c6023c77cfdb6580f82f8c31c1
cuong-wl <149576826+cuong-wl@users.noreply.github.com>
2024-03-06 11:33:09 +0700

    update

--- Mango/Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango/Mango.Services.CouponAPI/Models/Coupon.cs
@@ -6,6 +6,6 @@
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
-        public DateTime LastUpdated { get; set; }
+        public DateTime LastUpdated { get; set; } 
     }
 }
