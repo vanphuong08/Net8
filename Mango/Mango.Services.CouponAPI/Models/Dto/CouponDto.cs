a508482b738cb6480c816d83a5128091a8f350ae
cuong-wl <149576826+cuong-wl@users.noreply.github.com>
2024-03-06 17:51:37 +0700

    push code 06032024 Get Coupon

--- Mango/Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Mango/Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -3,8 +3,9 @@
     public class CouponDto
     {
         public int CouponID { get; set; }
-        public string CouponCode { get; set; }
+        public string? CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
 }
+ 
